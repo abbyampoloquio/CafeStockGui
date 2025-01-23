df3c23be3f3aca607eab49b5c2aef8ab318a1fc4
Gaming Edition IV <Gaming Edition IV@PC001>
2025-01-24 03:25:02 +0800

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Panel1 = New Panel()
+        Button1 = New Button()
+        PictureBox4 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        Label3 = New Label()
+        Panel4 = New Panel()
+        TextBox2 = New TextBox()
+        Panel3 = New Panel()
+        TextBox1 = New TextBox()
+        PictureBox1 = New PictureBox()
+        Panel2 = New Panel()
+        Label2 = New Label()
+        Label1 = New Label()
+        PictureBox2 = New PictureBox()
+        Label4 = New Label()
+        Label5 = New Label()
+        Panel1.SuspendLayout()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        Panel4.SuspendLayout()
+        Panel3.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        Panel2.SuspendLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackgroundImage = My.Resources.Resources.white
+        Panel1.BackgroundImageLayout = ImageLayout.Stretch
+        Panel1.Controls.Add(Label5)
+        Panel1.Controls.Add(Label4)
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(PictureBox4)
+        Panel1.Controls.Add(PictureBox3)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(Panel4)
+        Panel1.Controls.Add(Panel3)
+        Panel1.Controls.Add(PictureBox1)
+        Panel1.Controls.Add(Panel2)
+        Panel1.Dock = DockStyle.Fill
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(839, 548)
+        Panel1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Maroon
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Font = New Font("Trebuchet MS", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = Color.White
+        Button1.Location = New Point(619, 368)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(140, 34)
+        Button1.TabIndex = 7
+        Button1.Text = "Log in "
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.Image = My.Resources.Resources.newpass_removebg_preview
+        PictureBox4.Location = New Point(562, 300)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(39, 30)
+        PictureBox4.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox4.TabIndex = 6
+        PictureBox4.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = My.Resources.Resources.user_name_removebg_preview
+        PictureBox3.Location = New Point(563, 232)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(38, 30)
+        PictureBox3.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox3.TabIndex = 5
+        PictureBox3.TabStop = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(563, 204)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(79, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Administrator"
+        ' 
+        ' Panel4
+        ' 
+        Panel4.BackColor = Color.WhiteSmoke
+        Panel4.Controls.Add(TextBox2)
+        Panel4.Location = New Point(604, 300)
+        Panel4.Name = "Panel4"
+        Panel4.Size = New Size(180, 30)
+        Panel4.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.WhiteSmoke
+        TextBox2.BorderStyle = BorderStyle.None
+        TextBox2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox2.Location = New Point(3, 8)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(168, 22)
+        TextBox2.TabIndex = 0
+        TextBox2.Text = "password"
+        TextBox2.UseSystemPasswordChar = True
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = Color.WhiteSmoke
+        Panel3.Controls.Add(TextBox1)
+        Panel3.ForeColor = SystemColors.AppWorkspace
+        Panel3.Location = New Point(607, 232)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(177, 30)
+        Panel3.TabIndex = 2
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.WhiteSmoke
+        TextBox1.BorderStyle = BorderStyle.None
+        TextBox1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.ForeColor = Color.DimGray
+        TextBox1.Location = New Point(3, 8)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(171, 22)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "Username"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.Image = My.Resources.Resources.images_removebg_preview
+        PictureBox1.Location = New Point(560, 66)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(224, 71)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackgroundImage = My.Resources.Resources.red
+        Panel2.BackgroundImageLayout = ImageLayout.Stretch
+        Panel2.Controls.Add(Label2)
+        Panel2.Controls.Add(Label1)
+        Panel2.Controls.Add(PictureBox2)
+        Panel2.Dock = DockStyle.Left
+        Panel2.Location = New Point(0, 0)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(517, 548)
+        Panel2.TabIndex = 0
+        ' 
+        ' Label2
+        ' 
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Lucida Sans Unicode", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = SystemColors.ButtonHighlight
+        Label2.Location = New Point(81, 66)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(93, 57)
+        Label2.TabIndex = 3
+        Label2.Text = "Cafe Stock"
+        Label2.TextAlign = ContentAlignment.BottomCenter
+        ' 
+        ' Label1
+        ' 
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = SystemColors.ButtonHighlight
+        Label1.Location = New Point(12, 140)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(460, 390)
+        Label1.TabIndex = 2
+        Label1.Text = resources.GetString("Label1.Text")
+        Label1.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox2.Image = My.Resources.Resources.coffee_removebg_preview
+        PictureBox2.Location = New Point(0, 66)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(95, 71)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 4
+        PictureBox2.TabStop = False
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Transparent
+        Label4.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(821, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(15, 17)
+        Label4.TabIndex = 8
+        Label4.Text = "x"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.BackColor = Color.Transparent
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(798, 2)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(17, 15)
+        Label5.TabIndex = 9
+        Label5.Text = "__"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSizeMode = AutoSizeMode.GrowAndShrink
+        ClientSize = New Size(839, 548)
+        Controls.Add(Panel1)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        Panel4.ResumeLayout(False)
+        Panel4.PerformLayout()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        Panel2.ResumeLayout(False)
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub TextBox1_Click(sender As Object, e As EventArgs) Handles TextBox1.Click
+        While TextBox1.Text = "Username"
+            TextBox1.Text = ""
+        End While
+    End Sub
+
+    Private Sub TextBox2_Click(sender As Object, e As EventArgs) Handles TextBox2.Click
+        If TextBox2.Text = "password" Then
+            TextBox2.Text = ""
+        End If
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
